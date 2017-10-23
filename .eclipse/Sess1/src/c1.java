e7cc2e1763cb04d18424fc6cb3ebe4e07a8d801a
fa14-bse-056 <fa14-bse-056@LAB3A-10.cns.ciit>
2017-10-23 09:37:21 +0500

    sessionbal

---
+++ .eclipse/Sess1/src/c1.java
@@ -0,0 +1,6 @@
+
+public class c1 implements container {
+public int weight;
+public string[] itmlist={"itm1","itm2","itm3"};
+
+}
---
+++ .eclipse/Sess1/src/c2.java
@@ -0,0 +1,15 @@
+
+public class c2 implements iterator {
+public int weight;
+
+public boolean hasnext(){
+	if(index < name.length){
+	return true;
+}
+	return false;
+}
+
+
+
+
+}
